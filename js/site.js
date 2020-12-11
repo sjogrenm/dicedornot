d910d78728747c38bf5f3cb5d6e2c768849f7c1e
Calen Pennington <cale@edx.org>
2020-12-10 21:30:48 -0500

    Increase number of simulations

--- js/site.js
+++ js/site.js
@@ -129,7 +129,7 @@ function updateChart(rolls) {
       result.view.change('rolls', changeSet).run();
       $('#game-count').text(iteration);
       console.log(iteration, values.length);
-      if (iteration < 500) {
+      if (iteration < 3000) {
         window.setTimeout(addValues, 200);
       }
     }
