c6a721788810f0a3f80e5a7a308da49c79653272
Calen Pennington <cale@edx.org>
2021-02-16 11:30:23 -0500

    Improve replay loading from urls

--- src/Loading.svelte
+++ src/Loading.svelte
@@ -1,19 +1,25 @@
-<div class="progress">
-  <div
-    class="progress-bar progress-bar-striped progress-bar-animated active"
-    role="progressbar"
-    aria-valuenow="100"
-    aria-valuemin="0"
-    aria-valuemax="100"
-    style="width: 110%">
-    Loading...
+<script>
+  export let loading;
+</script>
+
+{#if loading}
+  <div class="progress">
+    <div
+      class="progress-bar progress-bar-striped progress-bar-animated active"
+      role="progressbar"
+      aria-valuenow="100"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style="width: 110%"
+    >
+      {loading}...
+    </div>
   </div>
-</div>
+{/if}
 
 <style>
   .progress {
     margin-top: 1em;
     height: 2em;
   }
-
 </style>
--- src/ReplayLoader.svelte
+++ src/ReplayLoader.svelte
@@ -3,31 +3,50 @@
   import { io } from "./io.js";
   import { replay, error } from "./stores.js";
   import { processReplay } from "./replay.js";
-  import { get, set, entries } from "idb-keyval";
+  import { get, set, entries, keys } from "idb-keyval";
   import Loading from "./Loading.svelte";
+import { key } from "vega";
 
   export let button = "primary",
-    loading = false;
+    loading = null;
   const CACHE_VERSION = 2;
 
   onMount(() => {
     if (!$replay) {
+      loadFromSearchParams();
+    }
+  });
+
+  let filePicker, urlPicker, cachePicker, replayKeys;
+  const rebblRE = /.*rebbl\.net\/rebbl\/match\/([0-9a-f]*)/i;
+  const goblinspyRE = /.*mordrek\.com\/gspy\/.*match\/([0-9a-f]*)/i
+  const spikeRE = /.*spike\.ovh\/match\?match_uuid=([0-9a-f]*)/i
+
+  async function loadFromSearchParams() {
       const params = new URLSearchParams(window.location.search);
+      if (!replayKeys) {
+        loading = "Loading saved replays";
+        replayKeys = await keys();
+        loading = null;
+      }
+      for (const [key, value] of params) {
+        let cacheKey = `${key}-${value}`;
+        if (replayKeys.includes(cacheKey)) {
+          loadFromCache(cacheKey);
+          return;
+        }
+      }
       const rebblUUID = params.get("rebbl");
       if (rebblUUID) {
         loadRebblReplay(rebblUUID);
+        return;
       }
       const gspyMID = params.get("gspy");
       if (gspyMID) {
         loadGoblinspyReplay(gspyMID);
+        return;
       }
-    }
-  });
-
-  let filePicker, urlPicker, cachePicker;
-  const rebblRE = /.*rebbl\.net\/rebbl\/match\/([0-9a-f]*)/i;
-  const goblinspyRE = /.*mordrek\.com\/gspy\/.*match\/([0-9a-f]*)/i
-  const spikeRE = /.*spike\.ovh\/match\?match_uuid=([0-9a-f]*)/i
+  }
 
   function loadURL() {
     if (!urlPicker.value) {
@@ -52,7 +71,18 @@
   }
 
   async function cachedReplays() {
-    const allReplays = await entries();
+    if (!replayKeys) {
+        loading = "Loading saved replays";
+        replayKeys = await keys();
+        loading = null;
+    }
+    const allReplays = [];
+    for (const key of replayKeys) {
+      loading = `Loading ${key}`;
+      const replay = await get(key);
+      allReplays.push([key, replay]) ;
+    }
+    loading = null;
     const validReplays = allReplays.filter(
       ([_, replay]) => replay.CACHE_VERSION === CACHE_VERSION
     );
@@ -75,46 +105,57 @@
   }
 
   async function loadFromCache(cacheKey) {
+    loading = `Loading ${cacheKey} from cache`;
+    console.log("Loading from cache", { cacheKey });
     const jsonReplayData = await get(cacheKey);
-    console.log("Loading from cache", { cacheKey, jsonReplayData });
+    console.log("Loaded", {jsonReplayData});
     if (jsonReplayData && jsonReplayData.CACHE_VERSION === CACHE_VERSION) {
       try {
+        let [replayType, ...replayId] = cacheKey.split('-');
+        replayId = replayId.join('-');
+        let shareURL = new URL(window.location);
+        shareURL.search = "";
+        shareURL.fragment = "";
+        shareURL.searchParams.set(replayType, replayId);
+        window.history.pushState({[replayType]: replayId}, "", shareURL.href)
         $replay = processReplay(jsonReplayData);
-        loading = false;
+        loading = null;
       } catch (err) {
-        loading = false;
+        loading = null;
         $error = err;
         console.error(err);
       }
     }
   }
 
-  async function parseReplay(replayFile, cacheKey, urlParams) {
-    console.log(urlParams);
+  async function parseReplay(replayFile, replayType, replayId) {
+    loading = `Parsing ${replayFile.name}`;
     io.xmlToJson(
       replayFile,
       function (jsonReplayData) {
         console.log("Preparing to process replay json...");
         jsonReplayData.Replay.filename = replayFile.name;
         jsonReplayData.CACHE_VERSION = CACHE_VERSION;
-        if (urlParams) {
-          let shareURL = new URL(window.location);
-          shareURL.search = urlParams;
-          jsonReplayData.Replay.url = shareURL.href;
-        }
+        let shareURL = new URL(window.location);
+        shareURL.search = "";
+        shareURL.fragment = "";
+        shareURL.searchParams.set(replayType, replayId);
+        window.history.pushState({[replayType]: replayId}, "", shareURL.href)
+        jsonReplayData.Replay.url = shareURL.href;
+        let cacheKey = `${replayType}-${replayId}`;
         set(cacheKey, jsonReplayData);
         console.log("Setting cache", { cacheKey, jsonReplayData });
         try {
           $replay = processReplay(jsonReplayData);
-          loading = false;
+          loading = null;
         } catch (err) {
-          loading = false;
+          loading = null;
           $error = err;
           console.error(err);
         }
       },
       function (err) {
-        loading = false;
+        loading = null;
         $error = err;
         alert(err);
       }
@@ -122,37 +163,39 @@
   }
 
   async function loadRebblReplay(uuid) {
-    loading = true;
+    loading = `Loading ${uuid} from rebbl.net`;
     $error = null;
-    let cacheKey = `rebbl-${uuid}`;
     let replayFile = await fetch(
       `https://rebbl.net/api/v2/match/${uuid}/replay`
     ).then((r) => r.json());
     if (replayFile.filename && replayFile.filename != "No replay file found") {
+      loading = `Downloading ${replayFile.filename}`;
       const blob = await fetch(replayFile.filename).then((r) => r.blob());
+      loading = null;
       const file = new File([blob], replayFile.filename);
-      parseReplay(file, cacheKey, `?rebbl=${uuid}`);
+      parseReplay(file, 'rebbl', uuid);
     } else {
       $error = `Unable to load replay for https://rebbl.net/rebbl/match/${uuid}`;
-      loading = false;
+      loading = null;
     }
   }
 
   async function loadGoblinspyReplay(mid) {
-    loading = true;
+    loading = `Loading ${mid} from GoblinSpy`;
     $error = null;
-    let cacheKey = `gspy-${mid}`;
     let replayFile = await fetch(
       `https://www.mordrek.com:666/api/v1/match/${mid}/url`
     ).then((r) => r.json());
     console.log("Replayfile", {replayFile});
     if (replayFile) {
+      loading = `Downloading ${replayFile}`;
       const blob = await fetch(replayFile).then((r) => r.blob());
+      loading = null;
       const file = new File([blob], replayFile);
-      parseReplay(file, cacheKey, `?gspy=${mid}`);
+      parseReplay(file, 'gspy', mid);
     } else {
       $error = `Unable to load replay for https://www.mordrek.com/gspy/match/${mid}. Try again later.`;
-      loading = false;
+      loading = null;
     }
   }
 
@@ -162,7 +205,7 @@
     $error = null;
     const files = filePicker.files;
     if (files.length > 0) {
-      parseReplay(files[0], `file-${files[0].name}`);
+      parseReplay(files[0], 'file', files[0].name);
     }
   }
 </script>
@@ -226,9 +269,7 @@
       </div>
     </div>
   {/await}
-  {#if loading}
-    <Loading />
-  {/if}
+  <Loading {loading}/>
 </div>
 
 <style>
