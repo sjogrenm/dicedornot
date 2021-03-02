1ee8e070f7fa0c5c315a14539c32031af56d3020
Calen Pennington <cale@edx.org>
2021-03-02 11:40:34 -0500

    Add missing Dice.svelte file

---
+++ src/Dice.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { BLOCK } from "./constants.js";
+  export let dice,
+    uphill = false,
+    size = 1.45,
+    separator = "";
+
+  let DICE_MAP = {
+    1: "a",
+    2: "b",
+    3: "c",
+    4: "d",
+    5: "e",
+    6: "f",
+    7: "g",
+    8: "h",
+    9: "k",
+    [BLOCK.AttackerDown]: "k",
+    [BLOCK.BothDown]: "n",
+    [BLOCK.Push]: "j",
+    [BLOCK.DefenderStumbles]: "m",
+    [BLOCK.DefenderDown]: "l",
+  };
+</script>
+
+{#each dice as die, dieIndex}
+  <span class="die" class:uphill style={`font-size: ${size}rem`}
+    >{DICE_MAP[die]}</span
+  >{#if dieIndex + 1 < dice.length}<span class="separator">{separator}</span
+    >{/if}
+{/each}
+
+<style>
+  span.die {
+    background: white;
+    color: black;
+    font-family: "NuffleDice";
+    vertical-align: middle;
+    line-height: 75%;
+  }
+  span.die.uphill {
+    background: red;
+  }
+</style>
