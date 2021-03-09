e41b0bf28263c9e489c81a8470e9fef5b3353ea9
Calen Pennington <cale@edx.org>
2021-03-09 15:13:51 -0500

    Revert "Attempt 2 to fix github-pages snowpack issues"
    This reverts commit 4c48246bebd17678dfb30a7416637605821cfae9.

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  buildOptions: {
-    baseUrl: './',
+    public: { url: './', static: true },
+    src: { url: './dist' },
   },
   plugins: [
     [
