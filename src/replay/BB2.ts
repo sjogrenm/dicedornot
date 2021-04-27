29e76636eeae093438aae91b87e3b1e987f955de
Calen Pennington <calen.pennington@gmail.com>
2021-04-27 09:02:06 -0400

    Handle JumpUp in blocks

--- src/replay/BB2.ts
+++ src/replay/BB2.ts
@@ -623,7 +623,7 @@ export interface ResultsAction<T> extends BaseAction {
 // Move = 0, //Move
 export interface MoveAction extends PlayerAction, OrderAction, ResultsAction<NoChoicesResult | DodgeResult | DodgePickResult | NoRollDodgeResult | GFIResult | LonerResult | StandUpResult | DivingTackleResult | TentaclesResult> { }
 // Block = 1, //Block
-export interface BlockAction extends PlayerAction, OrderAction, ResultsAction<BlockResult | PushResult | FollowUpResult | FoulAppearanceResult | DauntlessResult | StandFirmResult | WrestleResult | LonerResult | DodgePickResult | ChainsawKickbackResult> { ActionType: ACTION_TYPE.Block }
+export interface BlockAction extends PlayerAction, OrderAction, ResultsAction<BlockResult | PushResult | FollowUpResult | FoulAppearanceResult | DauntlessResult | StandFirmResult | WrestleResult | LonerResult | DodgePickResult | ChainsawKickbackResult | JumpUpResult> { ActionType: ACTION_TYPE.Block }
 // Blitz = 2, //Blitz
 export interface BlitzAction extends PlayerAction, OrderAction, ResultsAction<DodgePickResult | BlockResult | PushResult | FollowUpResult | FoulAppearanceResult | DauntlessResult | StandFirmResult | WrestleResult | LonerResult | NoChoicesResult | DodgeResult | NoRollDodgeResult | GFIResult | LonerResult | StandUpResult | DivingTackleResult | JuggernautResult | TentaclesResult | ChainsawKickbackResult> { ActionType: ACTION_TYPE.Blitz }
 // Pass = 3, //Pass
