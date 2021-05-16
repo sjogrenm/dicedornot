d17557e2e96485d7e3b227af36c61d0a207be4bc
Calen Pennington <calen.pennington@gmail.com>
2021-05-15 20:03:54 -0400

    Type AwayBench

--- src/viewer/AwayBench.svelte
+++ src/viewer/AwayBench.svelte
@@ -1,19 +1,45 @@
 <script lang="ts">
-  import CasBox from './CasBox.svelte';
-  import {SIDE} from '../constants.js';
-  export let pitchPlayers, dugout, send, receive;
+  import CasBox from "./CasBox.svelte";
+  import { SIDE } from "../constants.js";
+  import type * as Internal from "../replay/Internal.js";
+  import type { CrossFadeFn, Dugout, PlayerProps } from "./types.js";
+  export let pitchPlayers: Record<Internal.PlayerNumber, PlayerProps>,
+    dugout: Dugout,
+    send: CrossFadeFn,
+    receive: CrossFadeFn;
 </script>
 
 <div class="bench">
-  <CasBox {pitchPlayers} team={SIDE.away} casType="reserve" {dugout} {send} {receive}/>
-  <CasBox {pitchPlayers} team={SIDE.away} casType="ko" {dugout} {send} {receive}/>
-  <CasBox {pitchPlayers} team={SIDE.away} casType="cas" {dugout} {send} {receive}/>
+  <CasBox
+    {pitchPlayers}
+    team={SIDE.away}
+    casType="reserve"
+    {dugout}
+    {send}
+    {receive}
+  />
+  <CasBox
+    {pitchPlayers}
+    team={SIDE.away}
+    casType="ko"
+    {dugout}
+    {send}
+    {receive}
+  />
+  <CasBox
+    {pitchPlayers}
+    team={SIDE.away}
+    casType="cas"
+    {dugout}
+    {send}
+    {receive}
+  />
 </div>
 
 <style>
   .bench {
-    width: calc(600/1338*100%);
-    right: calc((1338 - 1316)/1338*100%);
+    width: calc(600 / 1338 * 100%);
+    right: calc((1338 - 1316) / 1338 * 100%);
     bottom: 5%;
     position: absolute;
     display: grid;
